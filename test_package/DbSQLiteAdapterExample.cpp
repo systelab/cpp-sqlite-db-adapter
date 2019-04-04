0387db84111c1c77ea6aa112c68f9bfb7449475a
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-04-04 22:09:33 +0200

    Added test for conan package

---
+++ test_package/DbSQLiteAdapterExample.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdexcept>
+
+#include "DbSQLiteAdapter/Connection.h"
+#include "DbSQLiteAdapter/ConnectionConfiguration.h"
+
+#include "DbAdapterInterface/IDatabase.h"
+
+
+int main(int argc, char *argv[])
+{
+	systelab::db::sqlite::ConnectionConfiguration configuration("./testdb.db");
+	systelab::db::sqlite::Connection connection;
+	std::unique_ptr<systelab::db::IDatabase> database = connection.loadDatabase(configuration);
+	
+	std::cout << "Database connection established successfully" << std::endl;
+
+    return 0;
+}
