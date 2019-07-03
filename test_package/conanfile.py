03979975c02a34fd318ea7e3ed3481109a0c127e
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-07-03 23:48:33 +0200

    Copy imported binaries considering build_type subfolder

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -12,9 +12,9 @@ class DbSQLiteAdapterTestConan(ConanFile):
         cmake.build()
 
     def imports(self):
-        self.copy("*.dll", dst="bin", src="bin")
-        self.copy("*.dylib*", dst="bin", src="lib")
-        self.copy('*.so*', dst='bin', src='lib')
+        self.copy("*.dll", dst=("bin/%s" % self.settings.build_type), src="bin")
+        self.copy("*.dylib*", dst=("bin/%s" % self.settings.build_type), src="lib")
+        self.copy('*.so*', dst=("bin/%s" % self.settings.build_type), src='lib')
 
     def test(self):
         cmake = CMake(self)
