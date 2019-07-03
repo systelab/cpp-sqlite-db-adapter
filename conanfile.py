e3e3be43cb386998601918c2c8dab0b86fbd56f6
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-07-03 23:11:51 +0200

    Moved to DbAdapter/1.1.1

--- conanfile.py
+++ conanfile.py
@@ -12,7 +12,7 @@ class DbSQLiteAdapterConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
 
     def requirements(self):
-        self.requires("DbAdapter/1.1.0@systelab/stable")
+        self.requires("DbAdapter/1.1.1@systelab/stable")
 
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin")
