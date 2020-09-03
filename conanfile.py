a12f2ea7ef8233925e629a8824ca9666408e6fae
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-09-03 12:00:19 +0200

    Moved to version 1.1.10 of DbAdapter

--- conanfile.py
+++ conanfile.py
@@ -20,10 +20,10 @@ class DbSQLiteAdapterConan(ConanFile):
         self.options["DbAdapterTestUtilities"].boost = self.options.boost
 
     def requirements(self):
-        self.requires("DbAdapterInterface/1.1.9@systelab/stable")
+        self.requires("DbAdapterInterface/1.1.10@systelab/stable")
 
     def build_requirements(self):
-        self.build_requires("DbAdapterTestUtilities/1.1.9@systelab/stable")
+        self.build_requires("DbAdapterTestUtilities/1.1.10@systelab/stable")
         if self.options.gtest == "1.7.0":
             self.build_requires("gtest/1.7.0@systelab/stable")
         elif self.options.gtest == "1.8.1":
