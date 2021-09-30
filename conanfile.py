6faf6d3a98721d1ec14cad416103ed276ecb00db
Josep Vila Badillo <jvila1@werfen.com>
2021-09-30 16:33:03 +0200

    Updated conan files to accept boost 1.76.0 Updated DbAdapterInterface to a preproduction version until it can be changed to the stable one.

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ class DbSQLiteAdapterConan(ConanFile):
     generators = "cmake_find_package"
     settings = "os", "compiler", "build_type", "arch"
     options = {"boost": ["1.66.0", "1.67.0", "1.72.0", "1.75.0", "1.76.0"], "gtest": ["1.7.0", "1.8.1", "1.10.0"]}
-    default_options = {"boost":"1.72.0", "gtest":"1.10.0"}
+    default_options = {"boost":"1.76.0", "gtest":"1.10.0"}
     exports_sources = "*"
 
     def configure(self):
@@ -20,16 +20,16 @@ class DbSQLiteAdapterConan(ConanFile):
         self.options["DbAdapterTestUtilities"].boost = self.options.boost
 
     def requirements(self):
-        self.requires("DbAdapterInterface/1.1.13@systelab/stable")
+        self.requires("DbAdapterInterface/1.1.13.1@systelab/testing")
 
     def build_requirements(self):
-        self.build_requires("DbAdapterTestUtilities/1.1.13@systelab/stable")
+        self.build_requires("DbAdapterTestUtilities/1.1.13.1@systelab/testing")
         if self.options.gtest == "1.7.0":
             self.build_requires("gtest/1.7.0@systelab/stable")
         elif self.options.gtest == "1.8.1":
             self.build_requires("gtest/1.8.1")
         else:
-            self.build_requires("gtest/1.10.0")
+            self.build_requires("gtest/1.10.0#0c895f60b461f8fee0da53a84d659131")
 
     def build(self):
         cmake = CMake(self)
